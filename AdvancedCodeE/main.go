aeff4dd289b6609d7ba7e18b622efe5de199fddf
Narciso III Javier <renzoj156@gmail.com>
2025-11-13 10:58:58 +0800

    For Advanced Coding

--- AdvancedCodeE/main.go
+++ AdvancedCodeE/main.go
@@ -1,1 +1,22 @@
-package AdvancedCodeE
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func greet(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
+func main() {
+	//Basic Completion
+	message := greet("Renzo")
+	fmt.Println(message)
+
+	//Type-aware
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(nums)
+
+}
